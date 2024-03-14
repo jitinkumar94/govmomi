195d59678a1b721583c4cdd9d1a2b89cdb7781d2
jparappurath <jparappurath@vmware.com>
2024-03-14 17:50:28 +0530

    Add DownloadEndpoint field in the file struct of content library

--- vapi/library/library_file.go
+++ vapi/library/library_file.go
@@ -36,6 +36,7 @@ type File struct {
 	Name     string    `json:"name,omitempty"`
 	Size     *int64    `json:"size,omitempty"`
 	Version  string    `json:"version,omitempty"`
+	DownloadEndpoint string `json:"file_download_endpoint,omitempty"`
 }
 
 // ListLibraryItemFiles returns a list of all the files for a library item.
